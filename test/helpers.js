588f043d4580603d905e09724c1f347aea23de41
Kiko Beats <josefrancisco.verdu@gmail.com>
2024-03-23 23:24:20 +0100

    test: refactor descriptions (#114)

--- test/helpers.js
+++ test/helpers.js
@@ -6,16 +6,18 @@ const { createServer } = require('http')
 const closeServer = server =>
   require('util').promisify(server.close.bind(server))()
 
-const runServer = async (t, handler) => {
+const runServer = async (t, handler, { throwErrors = true } = {}) => {
   const server = createServer(async (req, res) => {
     try {
       await handler({ req, res })
     } catch (error) {
       console.error(error)
+      if (throwErrors) throw error
       res.statusCode = 500
       res.end()
     }
   })
+
   const url = await listen(server)
   t.teardown(() => closeServer(server))
   return url
